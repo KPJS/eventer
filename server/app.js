eda40861f8d59dfd366653392a788f74f72162c4
Kučečka Tomáš (ERNI) <kuto@erni.sk>
2017-06-13 18:24:26 +0200

    hello world new

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var http = require("http");
 http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
-   response.end('Hello World\n');
+   response.end('Hello World New\n');
 }).listen(process.env.PORT || 1234);
 
 console.log('Server running');
